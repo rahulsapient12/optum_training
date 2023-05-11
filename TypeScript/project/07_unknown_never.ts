c183ee0a58c17b50b4db8824fec65dcf2d51eada
rahulsapient12 <rahul.nayak@publicissapient.com>
2023-05-11 13:30:05 +0530

    Typescript data types all

---
+++ TypeScript/project/07_unknown_never.ts
@@ -0,0 +1,31 @@
+// unknow type
+let val: unknown;
+console.log(val);
+val = true;
+console.log(val);
+val = 7;
+console.log(val);
+val = "geeks for geeks";
+console.log(val);
+val = [1, 2, 3, 4];
+console.log(val);
+val = { name: "rachel" };
+console.log(val);
+val = Math.random();
+console.log(val);
+val = null;
+console.log(val);
+val = undefined;
+
+// Never Type
+function throwError(errorMsg: string): never {
+  throw new Error(errorMsg);
+}
+
+function keepProcessing(): never {
+  while (true) {
+    console.log("I always does something and never ends.");
+  }
+}
+// The never type is used when you are sure that something is never going to occur. For example, you write a function which will not return to its end point or always throws an exception.
+
