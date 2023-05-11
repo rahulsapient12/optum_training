c8d50e011040553ddda480e7badf0c23908b93d3
rahulsapient12 <rahul.nayak@publicissapient.com>
2023-05-11 10:42:32 +0530

    Why TS?

--- TypeScript/00_why_TS.js
+++ TypeScript/00_why_TS.js
@@ -5,7 +5,13 @@ const button = document.getElementById("my_btn");
 function add(x, y) {
   return x + y;
 }
+function addWithCheck(x, y) {
+  if (typeof parseInt(x) === "number" && typeof parseInt(y) === "number") {
+    return parseInt(x) + parseInt(y);
+  }
+}
 
 button.addEventListener("click", () => {
-  console.log(add(num1.value, num2.value));
+  console.log(add(num1.value, num2.value), "without Check");
+  console.log(addWithCheck(num1.value, num2.value), "with Check");
 });
